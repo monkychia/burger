35ede097c682446d4fc021690f7fe333ba545a34
Catherine Chan <learnafew@gmail.com>
2018-08-25 10:08:12 -0700

    Added JAWSDB connection

--- config/connection.js
+++ config/connection.js
@@ -4,13 +4,39 @@ let sqlKeys = keys.sql;
 let mysql = require("mysql");
 
 // Set up our connection information
-let connection = mysql.createConnection({
-  host: "localhost",
-  port: 3306,
-  user: "root",
-  password: sqlKeys.dbPassword,
-  database: "burgers_db"
-});
+let connection;
+
+if (process.env.JAWSDB_URL) {
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+    connection = mysql.createConnection({
+        host: "localhost",
+        port: 3306,
+        user: "root",
+        password: sqlKeys.dbPassword,
+        database: "burgers_db"
+    });
+};
+
+// if (process.env.NODE_ENV === "development") {
+//     connection  = mysql.createConnection({
+//         host: "localhost",
+//         port: 3306,
+//         user: "root",
+//         password: sqlKeys.dbPassword,
+//         database: "burgers_db"
+//       });
+// } else {
+//     connection = mysql.createConnection()
+// }
+    
+//     = mysql.createConnection({
+//   host: "localhost",
+//   port: 3306,
+//   user: "root",
+//   password: sqlKeys.dbPassword,
+//   database: "burgers_db"
+// });
 
 // Connect to the database
 connection.connect(function(err) {
